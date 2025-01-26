607b13dab824839d8b9fe749e8ceee1e6b9d174f
Jean Barcellos <jeanbarcellos@hotmail.com>
2025-01-25 22:34:28 -0300

    Add migrations

---
+++ service-api/src/main/resources/db/migration/V202501251921__initial_data.sql
@@ -0,0 +1,9 @@
+
+SET client_encoding TO utf8;
+
+INSERT INTO project111.user (id, name, email, password, created_at)
+VALUES
+    ('5a05e2f6-1871-4ec0-8bd2-91a978f8d3c5', 'Usuário Teste 001', 'usuario001@test.com', '$2a$10$rIw0H6cXzblKeHJuf9usfuNKDCIFrr2nFszv2uRI6QAhhKX6dyW5G', CURRENT_TIMESTAMP),
+    ('22c5ccbc-7d5e-4654-b30d-4e85593f9b0c', 'Usuário Teste 002', 'usuario002@test.com', '$2a$10$rIw0H6cXzblKeHJuf9usfuNKDCIFrr2nFszv2uRI6QAhhKX6dyW5G', CURRENT_TIMESTAMP),
+    ('ef6b698e-02c2-4e24-bdd6-db21a2802ff9', 'Usuário Teste 003', 'usuario003@test.com', '$2a$10$rIw0H6cXzblKeHJuf9usfuNKDCIFrr2nFszv2uRI6QAhhKX6dyW5G', CURRENT_TIMESTAMP)
+;
